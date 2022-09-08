75d6f75553237bf5b0a449b1128e92522801f2d9
Your Name~ <f.zihad333@gmail.com>
2022-09-09 01:26:10 +0600

    Enter Key Pressing Search functionality added

--- js/index.js
+++ js/index.js
@@ -1,17 +1,24 @@
-const loadAPI = async (value) => {
+const loadAPI = async (value, limit) => {
+  toggleLoader(true);
   const url = `https://openapi.programming-hero.com/api/phones?search=${value}`;
   try {
     const res = await fetch(url);
     const data = await res.json();
-    display(data.data);
+    display(data.data, limit);
   } catch (error) {
     console.log(error);
   }
 };
-const display = (phones) => {
+const display = (phones, limit) => {
   const phoneBox = document.getElementById("phone-container");
   phoneBox.textContent = ``;
-  phones = phones.slice(0, 16);
+  const showBtn = document.getElementById("show-btn");
+  if (limit && phones.length > 12) {
+    phones = phones.slice(0, 12);
+    showBtn.classList.remove("d-none");
+  } else {
+    showBtn.classList.add("d-none");
+  }
   let errorMSg = document.getElementById("no-found-msg");
   console.log(errorMSg);
 
@@ -27,11 +34,11 @@ const display = (phones) => {
     col.classList.add("col");
     col.innerHTML = `
     <div class="card">
-        <img src="${phone.image}" class="card-img-top" alt="...">
+        <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
         <div class="card-body">
             <h5 class="card-title">${phone.phone_name}</h5>
             <p class="card-text">brand : ${phone.brand}</p>
-            <a href="#" class="btn btn-primary" id='${phone.slug}'>See Details</a>
+            <button class="btn btn-primary" onclick=displayDetailof('${phone.slug}')>See Details</button>
         </div>
     </div>
     `;
@@ -40,12 +47,22 @@ const display = (phones) => {
   toggleLoader(false);
 };
 
-document.getElementById("search-btn").addEventListener("click", function () {
-  toggleLoader(true);
+const doSearch = (limit) => {
   const searchInput = document.getElementById("input-text");
-  loadAPI(searchInput.value);
-  searchInput.value = "";
+  loadAPI(searchInput.value, limit);
+};
+
+document.getElementById("search-btn").addEventListener("click", function () {
+  doSearch(12);
 });
+
+document
+  .getElementById("input-text")
+  .addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      doSearch(12);
+    }
+  });
 
 const toggleLoader = (isLoading) => {
   const loader = document.getElementById("loader");
@@ -54,6 +71,17 @@ const toggleLoader = (isLoading) => {
   } else {
     loader.classList.add("d-none");
   }
+};
+
+document.getElementById("show-btn").addEventListener("click", function () {
+  doSearch();
+});
+
+const displayDetailof = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data.data);
 };
 
 loadAPI("iphone");
